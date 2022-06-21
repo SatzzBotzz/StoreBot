507bbad97bb3f6de2e096eebb466fddf7f09af40
kurniawan satria <kurniawansatria61@gmail.com>
2022-06-21 17:50:53 +0700

    By Satganz Devs

---
+++ settings.js
@@ -0,0 +1,131 @@
+//═══════════════════════════════════════════════════════//
+//
+//                              𝙶𝙾𝙹𝙾-𝚂𝙰𝚃𝙾𝚁𝚄 𝓫𝔂 𝓷𝓮𝔁𝓾𝓼𝓝𝔀
+//Satganz Devs : "gw cuman recode ya 😄👍"
+//
+//════════════════════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+
+//MAU YANG NO ENC? CHAT 6281316701742😷(FASTRESPON)\\
+global.produk1 = "SewaBot"
+global.produk2 = "Jasa Run Heroku"
+global.produk3 = "Jasa Upload Folder Ke Github"
+global.produk4 = "CustomBot"
+global.produk5 = "Personal Portfolio Web"
+global.produk6 = "Web Store"
+global.produk7 = "Sc  Bot Store"
+global.produk8 = "Tutorial Ngebot"
+//deskripsi produk😷\\
+global.desk1 =`┃╠ On 24jam
+┃╠ Fast Respon
+┃╠ Murah
+┃╠ Yakin Ga Sewa🗿`
+global.desk2 = `┃╠ Bot jadi fast respon
+┃╠ On 24j pastinya😷
+┃╠ Murah
+┃╠ Yakin Ga Order?🗿`
+global.desk3= `┃╠ 1000+ Folder
+┃╠ Cepat
+┃╠ Gege cuyyy
+┃╠ Yakin Ga Order?🗿`
+global.desk4 = `┃╠ Bisa Bikin Bot Store😷
+┃╠ No enc lah😷
+┃╠ Keren Pastinya
+┃╠ Yakin Ga Order?🗿`
+global.desk5 = `┃╠ Bisa Bikin web tentang kamu
+┃╠ Ez bat cuy
+┃╠ 1 template sih🗿
+┃╠ Yakin Ga Order?🗿`
+global.desk6 = `┃╠ Web kere
+┃╠ Ada Backsound🥶
+┃╠ Button ke we a kamu langsung🥶
+┃╠ Ada Foto kamu
+┃╠ Order dek`
+global.desk7 = `┃╠ Sc yang gw pake sekarang🥶`
+global.desk8 = `┃╠ Full Pengajaran
+┃╠  Di kasih termux mod
+┃╠  di kasih sc kalo perlu 
+┃╠ itu aja sih`
+//Harganya sayang\\
+global.harga1 = "Rp. 10.000"
+global.harga2 = "Rp. 10.000"
+global.harga3 = "Rp. 5.000"
+global.harga4 = "Rp. 20.000"
+global.harga5 = "Rp. 5.000"
+global.harga6 = "Rp. 10.000"
+global.harga7 = "Rp. 5.000"
+global.harga8 = "Rp. 10.000"
+//Payment Nya cuyy\\
+global.saweria = 'https://saweria.co/SatganzDevs'
+global.dana = "6281266891985"
+global.qris = 'https://saweria.co/overlays/qr?streamKey=7bd07b7576186f33dbdefbdb4e6a728f&backgroundColor=%23080808FF&barcodeColor=%23ffffffFF&username=satganzdevs'
+global.gopay = fs.readFileSync('./Media/gopay.jpg')
+
+//customize settings\\
+global.owner = ['6281316701742'] //put our number \\
+global.premium = ['6281266891985'] //put our number \\
+global.ownernomer = '6281316701742' //put our number \\
+global.ownername = 'Satganz Devs' //namakamu
+global.namatoko = 'Satganz Devs' //nama tokonyaa\\
+global.botname = 'Satganz Devs' //namakamu
+global.footer = '©Satganz Devs'//namakamu
+global.ig = 'https://instagram.com/kurniawansatria_24' //ig kamu\\
+global.region = 'Indonesia, Riau, Indragirihulu' //alamat kamu\\
+global.github = 'https://github.com/SatzzBotzz' //github kamu (jika punya)\\
+global.sc = 'https://github.com/SatzzBotzz/PersonalBot'
+global.myweb = 'https://satganz.kurniawansatria.repl.co' // webite kamu (jika punya)\\
+global.packname = 'Satganz Devs'
+global.author = 'Satganz Devs'
+global.sessionName = 'SatganzDevs'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'Wait A minute...',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./Media/thumb.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
